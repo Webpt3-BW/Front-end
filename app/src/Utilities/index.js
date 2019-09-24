85631da659ebbc79f0236774cb7e091dbd8fff55
baugher90 <45408433+baugher90@users.noreply.github.com>
2019-09-23 22:29:27 -0400

    folders and auth

---
+++ app/src/Utilities/index.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+export const config = {
+  apiUrl: "https://lambda-mud-test.herokuapp.com",
+  axiosWithAuth: function() {
+    return axios.create({
+      baseURL: this.apiUrl,
+      headers: {
+        Authorization: `Token ${localStorage.getItem("authToken")}`
+      }
+    });
+  }
+};
+
+export default config;
